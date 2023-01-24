627b11e86dcad5f8ebac284cb46f8ef99fc3291c
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-01-23 20:50:21 -0500

    first solution

---
+++ main.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector(".btn");
+const desktopBtn = document.querySelector(".desktop-btn");
+const shareBtn = document.querySelector(".share-btn");
+const user = document.querySelector(".user");
+const share = document.querySelector(".share-section");
+const svg = document.querySelector(".svg-path");
+const bubble = document.querySelector(".bubble-container");
+
+btn.addEventListener("click", () => {
+  user.parentNode.classList.toggle("hide");
+  share.parentNode.classList.toggle("open");
+});
+
+shareBtn.addEventListener("click", () => {
+  user.parentNode.classList.toggle("hide");
+  share.parentNode.classList.toggle("open");
+});
+
+desktopBtn.addEventListener("click", () => {
+  svg.classList.toggle("toggle-svg");
+  desktopBtn.classList.toggle("toggle-btn");
+  bubble.classList.toggle("bubble-open");
+});
