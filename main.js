db09772856aa61fc6310b3e4dabfccafa5cf70de
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-01-24 01:03:13 -0500

    simplified code by sharing elements with mobile and desktop

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 const btn = document.querySelector(".btn");
 const shareBtn = document.querySelector(".share-btn");
-const desktopBtn = document.querySelector(".desktop-btn");
 const user = document.querySelector(".user");
 const share = document.querySelector(".share-section");
 const svg = document.querySelector(".svg-path");
-const bubble = document.querySelector(".bubble-container");
 
 btn.addEventListener("click", () => {
   user.parentNode.classList.toggle("hide");
   share.parentNode.classList.toggle("open");
+  svg.classList.toggle("toggle-svg");
+  btn.classList.toggle("toggle-btn");
 });
 
 shareBtn.addEventListener("click", () => {
   user.parentNode.classList.toggle("hide");
   share.parentNode.classList.toggle("open");
-});
-
-desktopBtn.addEventListener("click", () => {
   svg.classList.toggle("toggle-svg");
-  desktopBtn.classList.toggle("toggle-btn");
-  bubble.classList.toggle("bubble-open");
+  btn.classList.toggle("toggle-btn");
 });
