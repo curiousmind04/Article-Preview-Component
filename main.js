0a48107f5ed245e08f7adffefe4ebe3d0255331b
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-01-24 12:05:54 -0500

    used less elements, simplified code

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 const btn = document.querySelector(".btn");
 const shareBtn = document.querySelector(".share-btn");
-const user = document.querySelector(".user");
 const share = document.querySelector(".share-section");
 const svg = document.querySelector(".svg-path");
 
 btn.addEventListener("click", () => {
-  user.parentNode.classList.toggle("hide");
   share.parentNode.classList.toggle("open");
   svg.classList.toggle("toggle-svg");
   btn.classList.toggle("toggle-btn");
 });
 
 shareBtn.addEventListener("click", () => {
-  user.parentNode.classList.toggle("hide");
   share.parentNode.classList.toggle("open");
   svg.classList.toggle("toggle-svg");
   btn.classList.toggle("toggle-btn");
